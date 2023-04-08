6d28a663cf99acbf2917f9f11e9b3b12e96d0a4a
asma es-sabbar <asma.es-sabbar@univ-fcomte.fr>
2023-04-08 04:13:35 +0200

    j

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,2 +1,20 @@
 <template>
+    <div class="flex justify-end mr-60 mt-40">
+    </div>
+    <div>
+        <h1 class="text-6xl font-extrabold text-center uppercase m-12 mr-48 ">ERREUR 404</h1>
+    </div>
+
+    <div>
+        <p class="text-left mt-36 ml-28 font-extrabold"> MMI Montbléliard <br />
+            2023
+        </p>
+    </div>
+    <div>
+
+    </div>
 </template>
+  
+<script>
+
+</script>
