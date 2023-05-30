eea8efa251cce05f298d5b5228440cfa186e6519
asma es-sabbar <asma.es-sabbar@univ-fcomte.fr>
2023-05-30 20:13:28 +0200

    bug

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -12,9 +12,10 @@
       <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-4">
 
         <a href="https://instagram.com/_asmaaes?igshid=OGQ5ZDc2ODk2ZA==">
-          <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl"
+          <div
+            class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl flex items-center justify-center"
             style="background-image:url('https://i.pinimg.com/originals/79/f3/b1/79f3b13eccd22129d8c6c06aa4210886.gif')">
-            <div class="absolute top-[360px] left-[200px] "> <svg width="90" height="90" viewBox="0 0 20 20" fill="none"
+            <div class=" absolute "> <svg width="90" height="90" viewBox="0 0 20 20" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M5.8 0H14.2C17.4 0 20 2.6 20 5.8V14.2C20 15.7383 19.3889 17.2135 18.3012 18.3012C17.2135 19.3889 15.7383 20 14.2 20H5.8C2.6 20 0 17.4 0 14.2V5.8C0 4.26174 0.61107 2.78649 1.69878 1.69878C2.78649 0.61107 4.26174 0 5.8 0ZM5.6 2C4.64522 2 3.72955 2.37928 3.05442 3.05442C2.37928 3.72955 2 4.64522 2 5.6V14.4C2 16.39 3.61 18 5.6 18H14.4C15.3548 18 16.2705 17.6207 16.9456 16.9456C17.6207 16.2705 18 15.3548 18 14.4V5.6C18 3.61 16.39 2 14.4 2H5.6ZM15.25 3.5C15.5815 3.5 15.8995 3.6317 16.1339 3.86612C16.3683 4.10054 16.5 4.41848 16.5 4.75C16.5 5.08152 16.3683 5.39946 16.1339 5.63388C15.8995 5.8683 15.5815 6 15.25 6C14.9185 6 14.6005 5.8683 14.3661 5.63388C14.1317 5.39946 14 5.08152 14 4.75C14 4.41848 14.1317 4.10054 14.3661 3.86612C14.6005 3.6317 14.9185 3.5 15.25 3.5ZM10 5C11.3261 5 12.5979 5.52678 13.5355 6.46447C14.4732 7.40215 15 8.67392 15 10C15 11.3261 14.4732 12.5979 13.5355 13.5355C12.5979 14.4732 11.3261 15 10 15C8.67392 15 7.40215 14.4732 6.46447 13.5355C5.52678 12.5979 5 11.3261 5 10C5 8.67392 5.52678 7.40215 6.46447 6.46447C7.40215 5.52678 8.67392 5 10 5ZM10 7C9.20435 7 8.44129 7.31607 7.87868 7.87868C7.31607 8.44129 7 9.20435 7 10C7 10.7956 7.31607 11.5587 7.87868 12.1213C8.44129 12.6839 9.20435 13 10 13C10.7956 13 11.5587 12.6839 12.1213 12.1213C12.6839 11.5587 13 10.7956 13 10C13 9.20435 12.6839 8.44129 12.1213 7.87868C11.5587 7.31607 10.7956 7 10 7Z"
@@ -30,9 +31,10 @@
         </a>
 
         <a href=https://github.com/esasma>
-          <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl"
+          <div
+            class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl flex items-center justify-center"
             style="background-image:url('https://i.pinimg.com/originals/b6/3a/70/b63a70bc0d7b3b3f8fec8fd535f26784.gif')">
-            <div class="absolute top-[360px] left-[520px] "> <svg width="90" height="90" viewBox="0 0 16 16" fill="none"
+            <div class="absolute "> <svg width="90" height="90" viewBox="0 0 16 16" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd"
                   d="M7.976 0C5.86071 0.000265156 3.83214 0.840676 2.33641 2.33641C0.840676 3.83214 0.000265156 5.86071 0 7.976C0 11.498 2.3 14.483 5.431 15.56C5.823 15.609 5.969 15.364 5.969 15.168V13.798C3.768 14.288 3.279 12.722 3.279 12.722C2.936 11.792 2.398 11.547 2.398 11.547C1.664 11.058 2.446 11.058 2.446 11.058C3.229 11.107 3.67 11.89 3.67 11.89C4.404 13.113 5.529 12.77 5.97 12.575C6.018 12.037 6.263 11.695 6.459 11.499C4.697 11.303 2.838 10.618 2.838 7.535C2.838 6.655 3.131 5.969 3.67 5.382C3.62 5.235 3.327 4.404 3.768 3.327C3.768 3.327 4.453 3.131 5.969 4.159C6.605 3.963 7.291 3.914 7.976 3.914C8.661 3.914 9.346 4.012 9.982 4.159C11.499 3.132 12.184 3.327 12.184 3.327C12.624 4.404 12.33 5.235 12.281 5.431C12.8199 6.01808 13.1171 6.7871 13.113 7.584C13.113 10.667 11.253 11.303 9.493 11.499C9.786 11.743 10.031 12.232 10.031 12.966V15.168C10.031 15.364 10.177 15.608 10.569 15.56C12.155 15.0248 13.5327 14.0046 14.5073 12.6436C15.4818 11.2827 16.004 9.64989 16 7.976C15.951 3.572 12.38 0 7.976 0Z"
@@ -49,9 +51,10 @@
         </a>
 
         <a href="mailto:essabbarasma17@gmail.com">
-          <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl"
+          <div
+            class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl flex items-center justify-center"
             style="background-image:url('https://i.pinimg.com/originals/61/8c/9c/618c9c339964b122d606283ca0e56ea3.gif')">
-            <div class="absolute top-[365px] left-[840px] "> <svg width="90" height="80" viewBox="0 0 20 16" fill="none"
+            <div class="absolute "> <svg width="90" height="80" viewBox="0 0 20 16" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M2 16C1.45 16 0.979002 15.804 0.587002 15.412C0.195002 15.02 -0.000664969 14.5493 1.69779e-06 14V2C1.69779e-06 1.45 0.196002 0.979002 0.588002 0.587002C0.980002 0.195002 1.45067 -0.000664969 2 1.69779e-06H18C18.55 1.69779e-06 19.021 0.196001 19.413 0.588001C19.805 0.980001 20.0007 1.45067 20 2V14C20 14.55 19.804 15.021 19.412 15.413C19.02 15.805 18.5493 16.0007 18 16H2ZM10 9L2 4V14H18V4L10 9ZM10 7L18 2H2L10 7ZM2 4V2V14V4Z"
@@ -68,9 +71,10 @@
         </a>
 
         <a href=https://butmmi.thomasjeu.fr>
-          <div class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl"
+          <div
+            class="overflow-hidden bg-cover rounded-lg cursor-pointer h-72 w-72 group hover:shadow-2xl mb-20 flex items-center justify-center"
             style="background-image:url('https://i.pinimg.com/originals/f1/3b/09/f13b094c41dfa06aba683631cd36e1c6.gif')">
-            <div class="absolute top-[380px] left-[1145px] uppercase text-white text-bold text-6xl">
+            <div class="absolute uppercase text-white text-bold text-6xl">
               <h1> Mmi </h1>
             </div>
             <div
--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-  <div class="flex pt-16 items-center justify-center ">
+  <div class="flex pt-16 items-center justify-center mb-36 ">
     <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
 
 
